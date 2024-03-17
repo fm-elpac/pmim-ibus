81f71531ec37ddae07cd0df5c3910c2bdee0d9ef
secext2022 <secext2022@outlook.com>
2024-03-17 12:18:02 +0800

    添加(插件): 内置双拼方案和键盘布局

---
+++ ui-vue/src/v/插件/p.vue
@@ -0,0 +1,17 @@
+<script setup>
+import c页面 from "@/c/页面.vue";
+</script>
+
+<template>
+  <c页面 class="v-插件">
+    TODO
+
+    警告: 目前对插件并没有足够的安全机制 (权限限制),
+    所以请不要使用恶意插件 !
+
+    (插件)
+  </c页面>
+</template>
+
+<style scoped>
+</style>
---
+++ uis/README.md
@@ -0,0 +1,7 @@
+# 胖喵拼音内置皮肤
+
+- `pmim-uis-nc`: 暖橙
+
+- `pmim-uis-bl`: 冰蓝
+
+TODO
